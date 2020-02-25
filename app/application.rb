81aa973e0f2cad5a562762204a334c8496feb659
David Cantoran <davidcantoran36@gmail.com>
2020-02-25 19:00:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,9 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    num2 = Kernel.rand(1..20)
+    num1 = Kernel.rand(1..20)
+    
     resp.finish
   end
 
